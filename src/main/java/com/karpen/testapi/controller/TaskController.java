1ee67f274269e61840afe69d8964d26a6af76c68
karpen <ghhvvfghvg44@gmail.com>
2024-10-11 02:55:49 +0900

    Initial commit

--- src/main/java/com/karpen/testapi/controller/TaskController.java
+++ src/main/java/com/karpen/testapi/controller/TaskController.java
@@ -1,5 +1,33 @@
 package com.karpen.testapi.controller;
 
-public class TaskController {
+import com.karpen.testapi.model.Task;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final List<Task> tasks = new ArrayList<>();
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping
+    public List<Task> getAllTasks(){
+        return tasks;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody String description){
+        Task newTask = new Task(counter.incrementAndGet(), description);
+        tasks.add(newTask);
+        return newTask;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTask(@PathVariable Long id){
+        tasks.removeIf(task -> task.getId().equals(id));
+        return "Task with ID " + id + "deleted.";
+    }
 }
--- src/main/java/com/karpen/testapi/model/Task.java
+++ src/main/java/com/karpen/testapi/model/Task.java
@@ -1,5 +1,19 @@
 package com.karpen.testapi.model;
 
 public class Task {
-    
+    private Long id;
+    private String description;
+
+    public Task(Long id , String description){
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getDescription(){
+        return description;
+    }
 }
